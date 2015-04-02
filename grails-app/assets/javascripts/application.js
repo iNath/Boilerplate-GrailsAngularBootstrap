57bf46fa96dc3e62bf7ed1b0f8b562507c7ab187
Nathanael Bertrand <nbertrand@doc4web.com>
2015-04-02 16:36:39 +0200

    Nettoyage du jquery qui gère le spinner de l'ajax

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -10,13 +10,3 @@
 //= require jquery
 //= require_tree .
 //= require_self
-
-if (typeof jQuery !== 'undefined') {
-	(function($) {
-		$('#spinner').ajaxStart(function() {
-			$(this).fadeIn();
-		}).ajaxStop(function() {
-			$(this).fadeOut();
-		});
-	})(jQuery);
-}
